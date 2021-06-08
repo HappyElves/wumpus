551e2e09d3761e626ca82a67e92b0807e720584d
HappyElves <84687889+HappyElves@users.noreply.github.com>
2021-06-08 14:17:59 +1000

    Delete Software Design Game directory

--- Software Design Game/library/map.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from pygame.locals import *
-
-class Obstacle(pygame.sprite.Sprite):
-    def __init__(self, imagePath):
-        super().__init__()
-        if imagePath:
-            self.image = pygame.image.load(imagePath).convert
-        self.rect = self.image.get_rect(center=center)
